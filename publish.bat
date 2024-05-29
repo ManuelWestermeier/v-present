3b5c7417102879604cb1c80d057712efe82c6752
ManuelWestermeier <westermeier111@gmail.com>
2024-05-29 22:06:50 +0200

    update

--- publish.bat
+++ publish.bat
@@ -1,5 +1,5 @@
 git add .
-git commit -m "first commit"
+git commit -m "update"
 git branch -M main
 git remote add origin https://github.com/ManuelWestermeier/v-present.git
 git push -u origin main
