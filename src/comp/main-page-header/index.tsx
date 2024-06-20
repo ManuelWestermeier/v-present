7db2065261222190014d7c64475231e90e5a1a7c
ManuelWestermeier <westermeier111@gmail.com>
2024-06-20 16:36:17 +0200

    update

--- src/comp/main-page-header/index.tsx
+++ src/comp/main-page-header/index.tsx
@@ -5,7 +5,7 @@ function MainPageHeader() {
     <>
       <div className="main-header">
         <h1>v-present</h1>
-        <p>The Best Open Source Markdown Presentation Software Worldwide</p>
+        <p>The Best <a href="https://github.com/ManuelWestermeier/v-present" title="go to source code">Open Source</a> Markdown Presentation Software Worldwide</p>
         <img
           src="https://manuelwestermeier.github.io/v-present/logo.jpg"
           alt="logo"
